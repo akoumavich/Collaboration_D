239c4f89eb4c2e7b2cf5ee500cafe38fb5687a41
Barré Hugo <hugo.barre@student-cs.fr>
2024-06-04 16:53:11 +0200

    fin journee

--- sample_code_submission/HPO.py
+++ sample_code_submission/HPO.py
@@ -2,7 +2,11 @@ from sklearn.model_selection import RandomizedSearchCV
 from xgboost import XGBClassifier
 from sklearn.metrics import roc_auc_score
 from boosted_decision_tree import BoostedDecisionTree
-
+from scipy import stats
+from HiggsML.datasets import train_test_split
+from HiggsML.datasets import BlackSwan_public_dataset as public_dataset
+from sklearn.preprocessing import StandardScaler
+from feature_engineering import feature_engineering
 
 # Define global variable to store the best model
 best_model = None
@@ -11,8 +15,9 @@ param_dist_XGB = {'max_depth': stats.randint(3, 9), # default 6
                 'n_estimators': stats.randint(30, 300), #default 100
                 'learning_rate': stats.uniform(0.1, 0.5)}
 # Function to optimize hyperparameters
-def optimize_hyperparameters(x_train, y_train, weights_train, param_dist, cv=2, n_iter=10):
-    
+def optimize_hyperparameters(train_set, param_dist=param_dist_XGB, cv=2, n_iter=5):
+    global best_model
+    x_train,y_train,weights_train=feature_engineering(train_set['data']),feature_engineering(train_set['labels']),feature_engineering(train_set['weights'])
     gsearch = RandomizedSearchCV(
         estimator=XGBClassifier(),
         param_distributions=param_dist,
@@ -28,18 +33,26 @@ def optimize_hyperparameters(x_train, y_train, weights_train, param_dist, cv=2, 
 
 # Function to predict with the optimized model
 def predict_with_optimized_model(scaler, test_data, y_test, weights_test):
-    
+    global best_model
+
     if best_model is None:
         raise RuntimeError("Hyperparameter optimization not performed yet.")
     
-    test_data = scaler.transform(test_data)
-    y_pred_gs = best_model.best_estimator_.predict_proba(test_data)[:, 1]
+    y_pred_gs = best_model.predict(test_data)
     
     print("... corresponding score on test dataset AUC: ", roc_auc_score(y_true=y_test, y_score=y_pred_gs, sample_weight=weights_test))
-    print("... corresponding score on test dataset signif: ", significance_score(y_true=y_test, y_score=y_pred_gs, sample_weight=weights_test))
+    # print("... corresponding score on test dataset signif: ", significance_score(y_true=y_test, y_score=y_pred_gs, sample_weight=weights_test))
 
 # Example usage
 # Assuming param_dist_XGB, X_train, y_train, weights_train, scaler, X_test, y_test, and weights_test are defined
 
 # optimize_hyperparameters(X_train, y_train, weights_train, param_dist=param_dist_XGB, cv=2, n_iter=10)
 # predict_with_optimized_model(scaler, X_test, y_test, weights_test)
+
+data=public_dataset()
+data.load_train_set()
+data_set=data.get_train_set()
+train_set, test_set= train_test_split(data_set, test_size=0.2, random_state=42,reweight=True)
+optimize_hyperparameters(train_set)
+predict_with_optimized_model(StandardScaler(),feature_engineering(test_set['data']),feature_engineering(test_set['labels']),feature_engineering(test_set['weights']))
+
--- sample_code_submission/boosted_decision_tree.py
+++ sample_code_submission/boosted_decision_tree.py
@@ -39,65 +39,27 @@ class BoostedDecisionTree:
         return roc_auc_score(y_test,y_pred,sample_weight=self.get_weights(data_set))
 from HiggsML.datasets import BlackSwan_public_dataset as public_dataset
 
-threshholds=np.arange(0.0001,0.0011,0.0001)
-roc_auc=[]
-accuracy=[]
-data=public_dataset()
-data.load_train_set()
-data_set=data.get_train_set()
-train_set, test_set= train_test_split(data_set, test_size=0.2, random_state=42,reweight=True)
-for threshhold in threshholds:
-    model=BoostedDecisionTree(data_set,'XGBoost')
-    model.fit(train_set['data'],train_set['labels'],train_set['weights'],eval_metric=f"error@{threshhold}")
-    y_pred=model.predict(test_set['data'])
-    y_pred_binary=model.predict_binary(test_set['data'])
-    print(y_pred[:10])
-    roc_auc.append(model.auc_score(test_set['labels'],y_pred,test_set) )
-    accuracy.append(accuracy_score(test_set['labels'],y_pred_binary))
+# threshholds=np.arange(0.0001,0.0011,0.0001)
+# roc_auc=[]
+# accuracy=[]
+# data=public_dataset()
+# data.load_train_set()
+# data_set=data.get_train_set()
+# train_set, test_set= train_test_split(data_set, test_size=0.2, random_state=42,reweight=True)
+# for threshhold in threshholds:
+#     model=BoostedDecisionTree(data_set,'XGBoost')
+#     model.fit(train_set['data'],train_set['labels'],train_set['weights'],eval_metric=f"error@{threshhold}")
+#     y_pred=model.predict(test_set['data'])
+#     y_pred_binary=model.predict_binary(test_set['data'])
+#     print(y_pred[:10])
+#     roc_auc.append(model.auc_score(test_set['labels'],y_pred,test_set) )
+#     accuracy.append(accuracy_score(test_set['labels'],y_pred_binary))
 
 
 
-plt.plot(threshholds,accuracy)
-plt.title('Accuracy as a function of threshhold')
-plt.xlabel('Threshhold')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.show()
-
-###### ajout #####
-#fonction de significance:
-def amsasimov(s_in,b_in):
-        s=np.copy(s_in)
-        b=np.copy(b_in)
-        s=np.where( (b_in == 0) , 0., s_in)
-        b=np.where( (b_in == 0) , 1., b)
-
-        ams = np.sqrt(2*((s+b)*np.log(1+s/b)-s))
-        ams=np.where( (s < 0)  | (b < 0), np.nan, ams)
-        if np.isscalar(s_in):
-            return float(ams)
-        else:
-            return  ams
-
-def significance_vscore(y_true, y_score, sample_weight=None):
-        if sample_weight is None:
-            sample_weight = np.full(len(y_true), 1.)
-        bins = np.linspace(0, 1., 101)
-        s_hist, bin_edges = np.histogram(y_score[y_true == 1], bins=bins, weights=sample_weight[y_true == 1])
-        b_hist, bin_edges = np.histogram(y_score[y_true == 0], bins=bins, weights=sample_weight[y_true == 0])
-        s_cumul = np.cumsum(s_hist[::-1])[::-1]
-        b_cumul = np.cumsum(b_hist[::-1])[::-1]
-        significance=amsasimov(s_cumul,b_cumul)
-        max_value = np.max(significance)
-        return significance
-
-def significance_score(y_true, y_score, sample_weight=None):
-        max_value = np.max(significance_vscore(y_true, y_score, sample_weight))
-
-        return max_value
-fsignificance_score = sklearn.metrics.make_scorer(significance_score)
-Z = significance_score(y_true=y_test, y_score=y_pred_skgb)
-print (Z)
-
-    
-
+# plt.plot(threshholds,accuracy)
+# plt.title('Accuracy as a function of threshhold')
+# plt.xlabel('Threshhold')
+# plt.ylabel('Accuracy')
+# plt.legend()
+# plt.show()
