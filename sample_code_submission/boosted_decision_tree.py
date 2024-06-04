940a990c19fa4585b337b2d24f125ff677a27118
Akoum Mohsen <mohsen.akoum@student-cs.fr>
2024-06-04 17:10:15 +0200

    update

--- sample_code_submission/boosted_decision_tree.py
+++ sample_code_submission/boosted_decision_tree.py
@@ -4,11 +4,12 @@ from xgboost import XGBClassifier
 from sklearn.preprocessing import StandardScaler
 import warnings
 warnings.filterwarnings("ignore")
-from sklearn.metrics import roc_auc_score, accuracy_score
+from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score, f1_score
 import lightgbm as lgb
 from sklearn import ensemble
 from HiggsML.datasets import train_test_split
 import matplotlib.pyplot as plt
+from feature_engineering import feature_engineering
 classifiers={'XGBoost':XGBClassifier(),'lightgbm':lgb.LGBMClassifier(),'sklearnbdt':ensemble.HistGradientBoostingClassifier()}
 class BoostedDecisionTree:
     """
@@ -42,24 +43,31 @@ from HiggsML.datasets import BlackSwan_public_dataset as public_dataset
 threshholds=np.arange(0.0001,0.0011,0.0001)
 roc_auc=[]
 accuracy=[]
+precision=[]
+recall=[]
+f1=[]
 data=public_dataset()
 data.load_train_set()
 data_set=data.get_train_set()
+data_set['data']=feature_engineering(data_set['data'])
 train_set, test_set= train_test_split(data_set, test_size=0.2, random_state=42,reweight=True)
 for threshhold in threshholds:
     model=BoostedDecisionTree(data_set,'XGBoost')
-    model.fit(train_set['data'],train_set['labels'],train_set['weights'],eval_metric=f"error@{threshhold}")
+    model.fit(train_set['data'],train_set['labels'],train_set['weights'],eval_metric="error")
     y_pred=model.predict(test_set['data'])
-    y_pred_binary=model.predict_binary(test_set['data'])
-    print(y_pred[:10])
-    roc_auc.append(model.auc_score(test_set['labels'],y_pred,test_set) )
+    y_pred_binary=(y_pred>=threshhold).astype(int)
+    # roc_auc.append(model.auc_score(test_set['labels'],y_pred,test_set) )
     accuracy.append(accuracy_score(test_set['labels'],y_pred_binary))
+    precision.append(precision_score(test_set['labels'], y_pred_binary, average='macro'))
+    recall.append(recall_score(test_set['labels'], y_pred_binary, average='macro'))
+    f1.append(f1_score(test_set['labels'], y_pred_binary, average='macro'))
 
-
-
-plt.plot(threshholds,accuracy)
-plt.title('Accuracy as a function of threshhold')
+plt.plot(threshholds,accuracy,label='accuracy')
+plt.plot(threshholds,precision,label='precision')
+plt.plot(threshholds,recall,label='recall')
+plt.plot(threshholds,f1,label='f1-score')
+plt.title('Different metrics as a function of threshhold')
 plt.xlabel('Threshhold')
-plt.ylabel('Accuracy')
+plt.ylabel('Metric')
 plt.legend()
 plt.show()
