fca7099396a23d98996ec8cb6e73c89563975d84
Akoum Mohsen <mohsen.akoum@student-cs.fr>
2024-06-05 22:44:01 +0200

    update plots

--- sample_code_submission/boosted_decision_tree.py
+++ sample_code_submission/boosted_decision_tree.py
@@ -11,9 +11,10 @@ from HiggsML.datasets import train_test_split
 import matplotlib.pyplot as plt
 from feature_engineering import feature_engineering
 from HiggsML.datasets import BlackSwan_public_dataset as public_dataset
-from Score_train_test import compare_train_test
-classifiers={'XGBoost':XGBClassifier(learning_rate= 0.4273454179451379, max_depth= 7, n_estimators=240),'lightgbm':lgb.LGBMClassifier(learning_rate= 0.48382410001969056, max_depth= 4,n_estimators=265),'sklearnbdt':ensemble.HistGradientBoostingClassifier(learning_rate= 0.52135689131965, max_depth= 3,max_iter= 115,min_samples_leaf=2)}
+classifiers={'XGBoost':XGBClassifier(learning_rate= 0.4273454179451379, max_depth= 7, n_estimators=240),'lightgbm':lgb.LGBMClassifier(learning_rate= 0.48382410001969056, max_depth= 5,n_estimators=265),'sklearnbdt':ensemble.HistGradientBoostingClassifier(learning_rate= 0.52135689131965, max_depth= 3,max_iter= 115,min_samples_leaf=2)}
 pd.set_option('display.max_columns',100)
+from significance import *
+import time
 class BoostedDecisionTree:
     """
     This Dummy class implements a decision tree classifier
@@ -30,11 +31,23 @@ class BoostedDecisionTree:
         self.scaler.fit_transform(train_data)
         X_train_data = self.scaler.transform(train_data)
         if self.classifier=="XGBoost":
+            print('fitting XGBoot model')
+            start_time=time.time()
             self.model.fit(X_train_data, labels, weights, eval_metric=eval_metric)
+            end_time=time.time()
+            print(f"XGBoost model fitted in {end_time-start_time} s")
         if self.classifier=="lightgbm":
+            print('fitting Lightgbm model')
+            start_time=time.time()
             self.model.fit(X_train_data, labels, weights)
+            end_time=time.time()
+            print(f"Lightgbm model fitted in {end_time-start_time} s")
         if self.classifier=='sklearnbdt':
+            print('fitting skgb model')
+            start_time=time.time()
             self.model.fit(X_train_data, labels, weights)
+            end_time=time.time()
+            print(f"skgb model fitted in {end_time-start_time} s")
     def predict(self, test_data):
         test_data = self.scaler.transform(test_data)
         return self.model.predict_proba(test_data)[:,1]
@@ -45,36 +58,53 @@ class BoostedDecisionTree:
         return data_set['weights']
     def auc_score(self,y_test,y_pred,data_set):
         return roc_auc_score(y_test,y_pred,sample_weight=self.get_weights(data_set))
-    def significance_vscore(self,y_true, y_score, sample_weight=None):
-        if sample_weight is None:
-            sample_weight = np.full(len(y_true), 1.)
-        bins = np.linspace(0, 1., 101)
-        s_hist, bin_edges = np.histogram(y_score[y_true == 1], bins=bins, weights=sample_weight[y_true == 1],density=True)
-        b_hist, bin_edges = np.histogram(y_score[y_true == 0], bins=bins, weights=sample_weight[y_true == 0],density=True)
-        s_cumul = np.cumsum(s_hist[::-1])[::-1]
-        b_cumul = np.cumsum(b_hist[::-1])[::-1]
-        
-    #max_value = np.max(significance)
-        s=np.copy(s_cumul)
-        b=np.copy(b_cumul)
-        s=np.where( (b_cumul == 0) , 0., s)
-        b=np.where( (b_cumul == 0) , 1., b)
 
-        ams = np.sqrt(2*((s+b)*np.log(1+s/b)-s))
-        ams=np.where( (s < 0)  | (b < 0), np.nan, ams)
-        if np.isscalar(s_cumul):
-            return float(ams)
-        else:
-            return  ams
-    def significance_curve(self,y_true, y_score, sample_weight=None):
-        Z = self.significance_vscore(y_true, y_score,sample_weight)
-        x = np.linspace(0, 1, num=len(Z))
-        plt.plot(x, Z)
-        plt.title("BDT Significance")
-        plt.xlabel("Threshold")
-        plt.ylabel("Significance")
-        plt.legend()
-        plt.show()
     
     
 if __name__=='__main__':
+    data=public_dataset()
+    data.load_train_set()
+    data_set=data.get_train_set()
+    data_set["data"]=feature_engineering(data_set["data"])
+    train_set, test_set= train_test_split(data_set,test_size=0.2, random_state=42,reweight=True)
+    training_data, train_weights, y_train=train_set["data"], train_set["weights"], train_set["labels"]
+    valid_data, valid_weights, y_test=test_set["data"], test_set["weights"], test_set["labels"]
+    class_weights_train = (
+           train_weights[y_train == 0].sum(),
+            train_weights[y_train == 1].sum(),
+        )
+    train_weights[y_train == 0] *= (
+                max(class_weights_train) / class_weights_train[0])
+    train_weights[y_train == 1] *= (
+                max(class_weights_train) / class_weights_train[1])
+    xgb=BoostedDecisionTree(train_set,classifier="XGBoost")
+    lgb_model=BoostedDecisionTree(train_set,classifier="lightgbm")
+    skgb=BoostedDecisionTree(train_set,classifier="sklearnbdt")
+    xgb.fit(training_data,y_train,weights=train_weights)
+    lgb_model.fit(training_data,y_train,weights=train_weights)
+    skgb.fit(training_data,y_train,weights=train_weights)
+    y_pred_xgb=xgb.predict(valid_data)
+    y_pred_lgb=lgb_model.predict(valid_data)
+    y_pred_skgb=skgb.predict(valid_data)
+    Z_xgb=significance_vscore(y_test,y_pred_xgb,sample_weight=valid_weights)
+    Z_lgb=significance_vscore(y_test,y_pred_lgb,sample_weight=valid_weights)
+    Z_skgb=significance_vscore(y_test,y_pred_skgb,sample_weight=valid_weights)
+    threshold=np.linspace(0,1,num=len(Z_xgb))
+    plt.plot(threshold,Z_xgb,label='XGBoost')
+    plt.plot(threshold,Z_lgb,label='Lightgbm')
+    plt.plot(threshold,Z_skgb,label='SKlearn GBDT')
+    plt.xlabel('Threshold')
+    plt.ylabel('Significance')
+    plt.title('Significance Curve')
+    plt.legend()
+    plt.savefig("significance_curve_comparison")
+    plt.clf()
+    fig, ax=seperation_curve(y_test, y_pred_xgb, sample_weight=valid_weights,bins=30,classifier="XGBoost")
+    plt.savefig("Histogram of Scores fore XGBoost")
+    plt.clf()
+    fig, ax=seperation_curve(y_test, y_pred_lgb, sample_weight=valid_weights,bins=30,classifier="Lightgbm")
+    plt.savefig("Histogram of Scores fore Lightgbm")
+    plt.clf()
+    fig, ax=seperation_curve(y_test, y_pred_skgb, sample_weight=valid_weights,bins=30,classifier="SKlearn GBDT")
+    plt.savefig("Histogram of Scores fore SKlearn GBDT")
+    plt.clf()
--- sample_code_submission/significance.py
+++ sample_code_submission/significance.py
@@ -1,6 +1,5 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
-from boosted_decision_tree import BoostedDecisionTree
 import sklearn.metrics
 import matplotlib.pyplot as plt
 from HiggsML.datasets import train_test_split
@@ -36,40 +35,79 @@ def significance_vscore(y_true, y_score, sample_weight=None):
 
 def significance_score(y_true, y_score, sample_weight=None):
     max_value = np.max(significance_vscore(y_true, y_score, sample_weight))
-
     return max_value
 
+def significance_curve(y_true, y_score, sample_weight=None):
+    Z = significance_vscore(y_true, y_score,sample_weight)
+    print("Z:",Z)
+    x = np.linspace(0, 1, num=len(Z))
 
 
-data=public_dataset()
-data.load_train_set()
-data_set=data.get_train_set()
-model=BoostedDecisionTree(data_set,'XGBoost')
-train_set, test_set= train_test_split(data_set, test_size=0.2, random_state=42,reweight=True)
-class_weights_train = (
-           train_set['weights'][train_set['labels'] == 0].sum(),
-            train_set['weights'][train_set['labels'] == 1].sum(),
-        )
-train_set['weights'][train_set['labels'] == 0] *= (
-                max(class_weights_train) / class_weights_train[0])
-train_set['weights'][train_set['labels'] == 1] *= (
-                max(class_weights_train) / class_weights_train[1])
+    plt.plot(x, Z)
+    plt.title("BDT Significance")
+    plt.xlabel("Threshold")
+    plt.ylabel("Significance")
+    plt.legend()
+    plt.show()
 
-model.fit(feature_engineering(train_set['data']),train_set['labels'],train_set['weights'])
-y_pred=model.predict(feature_engineering(test_set['data']))
-print(y_pred)
-fsignificance_score = sklearn.metrics.make_scorer(significance_score)
-Z = significance_vscore(y_true=test_set["labels"], y_score=y_pred,sample_weight=test_set["weights"])
-print("Z:",Z)
-x = np.linspace(0, 1, num=len(Z))
+def seperation_curve(y_true, y_score, sample_weight=None,bins=30, classifier="XGBoost"):
+    dfall = pd.DataFrame(y_score, columns=["score"])
+
+    fig, ax = plt.subplots(figsize=(10, 6))
 
 
-plt.plot(x, Z)
-plt.title("BDT Significance")
-plt.xlabel("Threshold")
-plt.ylabel("Significance")
-plt.legend()
-plt.show()
+    dfall[y_true == 1].hist(
+        weights=sample_weight[y_true == 1],
+        density=True,
+        bins=bins,
+        label="signal",
+        color='r',
+        alpha=0.5,
+        ax=ax
+    )
+
+
+    dfall[y_true == 0].hist(
+        weights=sample_weight[y_true == 0],
+        density=True,
+        bins=bins,
+        label="background",
+        color='b',
+        alpha=0.5,
+        ax=ax
+    )
+
+    # Customization
+    ax.set_title(f'Histogram of Scores for {classifier}')
+    ax.set_xlabel('Score')
+    ax.set_ylabel('Density')
+    ax.legend()
+    ax.grid(True)
+
+    # Display the plot
+    return fig ,ax
+
+
+
+# data=public_dataset()
+# data.load_train_set()
+# data_set=data.get_train_set()
+# model=BoostedDecisionTree(data_set,'XGBoost')
+# train_set, test_set= train_test_split(data_set, test_size=0.2, random_state=42,reweight=True)
+# class_weights_train = (
+#            train_set['weights'][train_set['labels'] == 0].sum(),
+#             train_set['weights'][train_set['labels'] == 1].sum(),
+#         )
+# train_set['weights'][train_set['labels'] == 0] *= (
+#                 max(class_weights_train) / class_weights_train[0])
+# train_set['weights'][train_set['labels'] == 1] *= (
+#                 max(class_weights_train) / class_weights_train[1])
+
+# model.fit(feature_engineering(train_set['data']),train_set['labels'],train_set['weights'])
+# y_pred=model.predict(feature_engineering(test_set['data']))
+# print(y_pred)
+# fsignificance_score = sklearn.metrics.make_scorer(significance_score)
+
 
 # dfall=pd.DataFrame(y_pred,columns=["score"])
 # ax = dfall[test_set["labels"] == 1].hist(weights= test_set['weights'][test_set["labels"] == 1],density=True,bins=30,label="signal",color='r')
@@ -106,39 +144,4 @@ plt.show()
 # center = (bins[:-1] + bins[1:]) / 2
 # plt.legend(loc='best')
 # plt.errorbar(center, hist, yerr=err, fmt='o', c='b', label='B (test)')
-dfall = pd.DataFrame(y_pred, columns=["score"])
-
-# Create the figure and axis
-fig, ax = plt.subplots(figsize=(10, 6))
-
-# Plot the histogram for the signal
-dfall[test_set["labels"] == 1].hist(
-    weights=test_set['weights'][test_set["labels"] == 1],
-    density=True,
-    bins=30,
-    label="signal",
-    color='r',
-    alpha=0.5,
-    ax=ax
-)
-
-# Plot the histogram for the background
-dfall[test_set["labels"] == 0].hist(
-    weights=test_set['weights'][test_set["labels"] == 0],
-    density=True,
-    bins=30,
-    label="background",
-    color='b',
-    alpha=0.5,
-    ax=ax
-)
-
-# Customization
-ax.set_title('Histogram of Scores')
-ax.set_xlabel('Score')
-ax.set_ylabel('Density')
-ax.legend()
-ax.grid(True)
-
-# Display the plot
-plt.show()
+    
