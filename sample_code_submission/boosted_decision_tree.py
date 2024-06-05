bc612d48d3bf369ce667aaa6b242aa0e72435419
SETTI YASSINE <yassine.setti@student-cs.fr>
2024-06-05 10:26:15 +0200

    plots_and_organized_it

--- sample_code_submission/boosted_decision_tree.py
+++ sample_code_submission/boosted_decision_tree.py
@@ -3,6 +3,7 @@ import pandas as pd
 from xgboost import XGBClassifier
 from sklearn.preprocessing import StandardScaler
 import warnings
+from roc_curve import roc_curve_plot
 from featureengineering import feature_engineering as fe
 warnings.filterwarnings("ignore")
 from sklearn.metrics import roc_auc_score, accuracy_score
@@ -49,26 +50,7 @@ if __name__=='__main__':
     train_set, test_set= train_test_split(data_set, test_size=0.2, random_state=42,reweight=True)
     model=BoostedDecisionTree(data_set,'XGBoost')
     model.fit(train_set['data'],train_set['labels'],train_set['weights'])
-    y_pred=model.predict(test_set['data'])
-    y_pred_binary=model.predict_binary(test_set['data'])
-    print(y_pred[:10])
-    roc_score=model.auc_score(test_set['labels'],y_pred,test_set)
-    print('roc_auc_score :' ,model.auc_score(test_set['labels'],y_pred,test_set) )
-    accuracy=accuracy_score(test_set['labels'],y_pred_binary)
-    print(f"accuracy: {accuracy}")
-    from sklearn.metrics import roc_curve
-    fpr_xgb,tpr_xgb,_ = roc_curve(test_set['labels'],y_pred,sample_weight=test_set['weights'])
-    plt.plot(fpr_xgb, tpr_xgb, color='darkgreen',lw=2, label='XGBoost (AUC  = {:.3f})'.format(roc_score))
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('Background Efficiency')
-    plt.ylabel('Signal Efficiency')
-    plt.title('ROC curve')
-    plt.legend(loc="lower right")
-    plt.savefig("ROC_comparingnonoptimizedfe.pdf")
-    plt.show()
-
+    roc_curve_plot(model,test_set)
 
     
 
