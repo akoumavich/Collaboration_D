978c501684200757c4f8de29f9a9bc1d0cd44d7d
SETTI YASSINE <yassine.setti@student-cs.fr>
2024-06-05 09:45:32 +0200

    roc_curve_et_auc_score

--- sample_code_submission/boosted_decision_tree.py
+++ sample_code_submission/boosted_decision_tree.py
@@ -3,6 +3,7 @@ import pandas as pd
 from xgboost import XGBClassifier
 from sklearn.preprocessing import StandardScaler
 import warnings
+from featureengineering import feature_engineering as fe
 warnings.filterwarnings("ignore")
 from sklearn.metrics import roc_auc_score, accuracy_score
 import lightgbm as lgb
@@ -37,20 +38,36 @@ class BoostedDecisionTree:
         return data_set['weights']
     def auc_score(self,y_test,y_pred,data_set):
         return roc_auc_score(y_test,y_pred,sample_weight=self.get_weights(data_set))
-from HiggsML.datasets import BlackSwan_public_dataset as public_dataset
 
-data=public_dataset()
-data.load_train_set()
-data_set=data.get_train_set()
-train_set, test_set= train_test_split(data_set, test_size=0.2, random_state=42,reweight=True)
-model=BoostedDecisionTree(data_set,'XGBoost')
-model.fit(train_set['data'],train_set['labels'],train_set['weights'])
-y_pred=model.predict(test_set['data'])
-y_pred_binary=model.predict_binary(test_set['data'])
-print(y_pred[:10])
-print('roc_auc_score :' ,model.auc_score(test_set['labels'],y_pred,test_set) )
-accuracy=accuracy_score(test_set['labels'],y_pred_binary)
-print(f"accuracy: {accuracy}")
+if __name__=='__main__':
+    from HiggsML.datasets import BlackSwan_public_dataset as public_dataset
+
+    data=public_dataset()
+    data.load_train_set()
+    data_set=data.get_train_set()
+    data_set['data']=fe(data_set['data'])
+    train_set, test_set= train_test_split(data_set, test_size=0.2, random_state=42,reweight=True)
+    model=BoostedDecisionTree(data_set,'XGBoost')
+    model.fit(train_set['data'],train_set['labels'],train_set['weights'])
+    y_pred=model.predict(test_set['data'])
+    y_pred_binary=model.predict_binary(test_set['data'])
+    print(y_pred[:10])
+    roc_score=model.auc_score(test_set['labels'],y_pred,test_set)
+    print('roc_auc_score :' ,model.auc_score(test_set['labels'],y_pred,test_set) )
+    accuracy=accuracy_score(test_set['labels'],y_pred_binary)
+    print(f"accuracy: {accuracy}")
+    from sklearn.metrics import roc_curve
+    fpr_xgb,tpr_xgb,_ = roc_curve(test_set['labels'],y_pred,sample_weight=test_set['weights'])
+    plt.plot(fpr_xgb, tpr_xgb, color='darkgreen',lw=2, label='XGBoost (AUC  = {:.3f})'.format(roc_score))
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Background Efficiency')
+    plt.ylabel('Signal Efficiency')
+    plt.title('ROC curve')
+    plt.legend(loc="lower right")
+    plt.savefig("ROC_comparingnonoptimizedfe.pdf")
+    plt.show()
 
 
     
---
+++ sample_code_submission/featureengineering.py
@@ -0,0 +1,179 @@
+import pandas as pd
+import numpy as np
+
+
+def feature_engineering(df):
+    """
+    Perform feature engineering operations on the input dataframe
+    and create a new dataframe with only the features required for training the model.
+    """
+
+    # Perform calculations to derive features from the DataFrame
+    # and store the results in new columns
+
+    # Engineered features to be used for calculations
+
+    lep_px = df["PRI_lep_pt"] * np.cos(df["PRI_lep_phi"])
+    lep_py = df["PRI_lep_pt"] * np.sin(df["PRI_lep_phi"])
+    lep_pz = df["PRI_lep_pt"] * np.sinh(df["PRI_lep_eta"])
+
+    had_px = df["PRI_had_pt"] * np.cos(df["PRI_had_phi"])
+    had_py = df["PRI_had_pt"] * np.sin(df["PRI_had_phi"])
+    had_pz = df["PRI_had_pt"] * np.sinh(df["PRI_had_eta"])
+
+    met_x = df["PRI_met"] * np.cos(df["PRI_met_phi"])
+    met_y = df["PRI_met"] * np.sin(df["PRI_met_phi"])
+
+    n_jets = df["PRI_n_jets"]
+
+    jet_leading_px = df["PRI_jet_leading_pt"] * np.cos(df["PRI_jet_leading_phi"])
+    jet_leading_py = df["PRI_jet_leading_pt"] * np.sin(df["PRI_jet_leading_phi"])
+    jet_leading_pz = df["PRI_jet_leading_pt"] * np.sinh(df["PRI_jet_leading_eta"])
+
+    jet_subleading_px = df["PRI_jet_subleading_pt"] * np.cos(df["PRI_jet_subleading_phi"])
+    jet_subleading_py = df["PRI_jet_subleading_pt"] * np.sin(df["PRI_jet_subleading_phi"])
+    jet_subleading_pz = df["PRI_jet_subleading_pt"] * np.sinh(df["PRI_jet_subleading_eta"])
+
+    # Put -7 to NaN
+    for col in df:
+        df[col][df[col] == -7] = np.nan
+
+    # Engineered features to be passed on to the dataframe
+
+    # DER_mass_vis, formule 21
+    with np.errstate(invalid="ignore"):
+        df["DER_mass_vis"] = np.sqrt(
+            (
+                np.sqrt(lep_px**2 + lep_py**2 + lep_pz**2)
+                + np.sqrt(had_px**2 + had_py**2 + had_pz**2)
+            )
+            ** 2
+            - (lep_px + had_px) ** 2
+            - (lep_py + had_py) ** 2
+            - (lep_pz + had_pz) ** 2
+        )
+
+    # DER_pt_h, formule 20
+    with np.errstate(invalid="ignore"):
+        df["DER_pt_h"] = np.sqrt((lep_px + had_px + met_x) ** 2 + (lep_py + had_py + met_y) ** 2)
+
+    # DER_mass_transverse_met_lep, formule 22
+    with np.errstate(invalid="ignore"):
+        df["DER_mass_transverse_met_lep"] = np.sqrt(
+            (df["PRI_lep_pt"] + df["PRI_met"]) ** 2 - (lep_px + met_x) ** 2 - (lep_py + met_y) ** 2
+        )
+
+    # DER_mass_jet_jet, formule 21
+    with np.errstate(invalid="ignore"):
+        df["DER_mass_jet_jet"] = np.where(
+            n_jets >= 2,
+            np.sqrt(
+                (
+                    np.sqrt(jet_leading_px**2 + jet_leading_py**2 + jet_leading_pz**2)
+                    + np.sqrt(jet_subleading_px**2 + jet_subleading_py**2 + jet_subleading_pz**2)
+                )
+                ** 2
+                - (jet_leading_px + jet_subleading_px) ** 2
+                - (jet_leading_py + jet_subleading_py) ** 2
+                - (jet_leading_pz + jet_subleading_pz) ** 2
+            ),
+            np.nan,
+        )
+
+    # DER_deltaeta_jet_jet, formule 23
+    df["DER_deltaeta_jet_jet"] = np.abs(df["PRI_jet_leading_eta"] - df["PRI_jet_subleading_eta"])
+
+    # DER_sum_pt
+    df["DER_sum_pt"] = np.where(
+        n_jets == 0,
+        df["PRI_lep_pt"] + df["PRI_had_pt"],
+        np.where(
+            n_jets == 1,
+            df["PRI_lep_pt"] + df["PRI_had_pt"] + df["PRI_jet_leading_pt"],
+            np.where(
+                n_jets == 2,
+                df["PRI_lep_pt"]
+                + df["PRI_had_pt"]
+                + df["PRI_jet_leading_pt"]
+                + df["PRI_jet_subleading_pt"],
+                np.where(
+                    n_jets == 3, df["PRI_lep_pt"] + df["PRI_had_pt"] + df["PRI_jet_all_pt"], np.nan
+                ),
+            ),
+        ),
+    )
+
+    # DER_met_phi_centrality
+    A = np.sin(df["PRI_met_phi"] - df["PRI_lep_phi"]) * np.sign(
+        np.sin(df["PRI_had_phi"] - df["PRI_lep_phi"])
+    )
+    B = np.sin(df["PRI_had_phi"] - df["PRI_met_phi"]) * np.sign(
+        np.sin(df["PRI_had_phi"] - df["PRI_lep_phi"])
+    )
+    df["DER_met_phi_centrality"] = (A + B) / np.sqrt(A**2 + B**2)
+
+    # DER_mass_transverse_met_lep
+    df["DER_mass_transverse_met_lep"] = np.sqrt(
+        (df["PRI_lep_pt"] + df["PRI_met"]) ** 2 - (lep_px + met_x) ** 2 - (lep_py + met_y) ** 2
+    )
+
+    # DER_proeta_jet_jet
+    df["DER_proeta_jet_jet"] = df["PRI_jet_leading_eta"] * df["PRI_jet_subleading_eta"]
+
+    # DER_deltar_tau_lep
+    df["DER_deltar_tau_lep"] = np.sqrt(
+        (df["PRI_lep_phi"] - df["PRI_had_phi"]) ** 2 + (df["PRI_lep_eta"] - df["PRI_had_eta"]) ** 2
+    )
+
+    # DER_lep_eta_centrality
+    df["DER_lep_eta_centrality"] = np.where(
+        n_jets >= 2,
+        np.exp(
+            (
+                -4
+                * (
+                    df["PRI_lep_eta"]
+                    - (df["PRI_jet_leading_eta"] + df["PRI_jet_subleading_eta"]) / 2
+                )
+                ** 2
+            )
+            / (df["PRI_jet_leading_eta"] - df["PRI_jet_subleading_eta"]) ** 2
+        ),
+        np.nan,
+    )
+
+    # DER_pt_ratio_lep_tau
+    df["DER_pt_ratio_lep_tau"] = df["PRI_lep_pt"] / df["PRI_had_pt"]
+
+    # DER_pt_tot
+    df["DER_pt_tot"] = np.where(
+        n_jets == 2,
+        np.sqrt(
+            lep_pz**2
+            + had_pz**2
+            + jet_leading_pz**2
+            + jet_subleading_pz**2
+            + df["PRI_had_pt"] ** 2
+            + df["PRI_lep_pt"] ** 2
+            + df["PRI_jet_leading_pt"] ** 2
+            + df["PRI_jet_subleading_pt"] ** 2
+        ),
+        np.where(
+            n_jets >= 1,
+            np.sqrt(
+                lep_pz**2
+                + had_pz**2
+                + jet_leading_pz**2
+                + df["PRI_had_pt"] ** 2
+                + df["PRI_lep_pt"] ** 2
+                + df["PRI_jet_leading_pt"] ** 2
+            ),
+            np.nan,
+        ),
+    )
+
+    # Create the new dataframe
+    new_columns = df.columns.tolist()
+    df_new = pd.DataFrame(df, columns=new_columns)
+
+    return df_new
