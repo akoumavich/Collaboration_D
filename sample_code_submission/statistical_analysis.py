95e169332c38535c1d7c477f6b2c9f1731e781b2
Aurélien Hamy <aunetx@yandex.com>
2024-06-07 09:35:28 +0200

    beautify

--- sample_code_submission/statistical_analysis.py
+++ sample_code_submission/statistical_analysis.py
@@ -9,7 +9,7 @@ from numpy import NaN
 import numpy as np
 import pandas as pd
 
-os.chdir('D:/cours/centralesupelec/1A/ST 4/ST4 black swan/EI/Collaboration_D')
+os.chdir("D:/cours/centralesupelec/1A/ST 4/ST4 black swan/EI/Collaboration_D")
 
 
 def compute_mu(score, weight, saved_info):
@@ -22,9 +22,9 @@ def compute_mu(score, weight, saved_info):
     def n_bins(score, poids, c_alpha, result_fonc):
         ns, nb, proS, proB, poidsS, poidsB = result_fonc
         #  Définition des constantes utiles tout au long du programme
-        nombre_intervaux = len(proS)     # découpage de l'intervalle de score
+        nombre_intervaux = len(proS)  # découpage de l'intervalle de score
         # longueur de chaque intervalle de score
-        long_interv = 1/(nombre_intervaux-1)
+        long_interv = 1 / (nombre_intervaux - 1)
         # array de boolées du score:
 
         # création des histogrammes avec le poids
@@ -33,7 +33,7 @@ def compute_mu(score, weight, saved_info):
 
         nb_tot = ns + nb
 
-        ind_max = int(c_alpha/long_interv)
+        ind_max = int(c_alpha / long_interv)
         n_sign = np.zeros(len(nb_tot))
         n_bruit = np.zeros(len(nb_tot))
         for i in range(len(nb_tot)):
@@ -61,53 +61,59 @@ def compute_mu(score, weight, saved_info):
         # print(ll)
         # return ll
         for i in range(len(liste_n)):
-            lam = mu*NS*pS[i] + NB*pB[i]
+            lam = mu * NS * pS[i] + NB * pB[i]
             if lam != 0:
-                likel += -lam + liste_n[i]*math.log(lam)
+                likel += -lam + liste_n[i] * math.log(lam)
         return likel
 
     def minimisateur(liste_n, liste_nS, liste_nB, wS, wB):
         EPS = 0.0001  # trick to avoid potential division by zero during the minimization
         # Forbids parameter values to be negative, so mu>EPS here.
         par_bnds = (EPS, None)
-        return minimize(lambda mu: -2*likely(liste_n, liste_nS, liste_nB, pS, pB, wS, wB, mu), .5, bounds=par_bnds[1])
+        return minimize(
+            lambda mu: -2 * likely(liste_n, liste_nS, liste_nB, pS, pB, wS, wB, mu),
+            0.5,
+            bounds=par_bnds[1],
+        )
 
     def incertitude(liste_n, liste_nS, liste_nB, pS, pB, wS, wB):
         mu_axis_values = np.linspace(0.5, 1.5, 200)
 
-        loglike_values = np.array([-2*likely(liste_n, liste_nS, liste_nB, pS, pB, wS, wB, mu)
-                                   for mu in mu_axis_values]).flatten()
+        loglike_values = np.array(
+            [-2 * likely(liste_n, liste_nS, liste_nB, pS, pB, wS, wB, mu) for mu in mu_axis_values]
+        ).flatten()
         mini = minimisateur(liste_n, liste_nS, liste_nB, wS, wB).x[0]
         print(f"L'estimateur de mu chapeau vaut : {mini}")
 
-        plt.plot(mu_axis_values, loglike_values -
-                 min(loglike_values), label='log-likelihood')
-        plt.hlines(1, min(mu_axis_values), max(mu_axis_values),
-                   linestyle='--', color='tab:gray')
+        plt.plot(mu_axis_values, loglike_values - min(loglike_values), label="log-likelihood")
+        plt.hlines(1, min(mu_axis_values), max(mu_axis_values), linestyle="--", color="tab:gray")
 
         # This is the code to search for which mu values the log-likelihood ratio takes
         # the value 1. For this we pick up first the indexes:
-        idx = np.argwhere(
-            np.diff(np.sign(loglike_values-min(loglike_values)-1))).flatten()
+        idx = np.argwhere(np.diff(np.sign(loglike_values - min(loglike_values) - 1))).flatten()
 
         # idx = np.append(idx, 50)
 
         # and we plot then the position of the mu values:
-        plt.plot(mu_axis_values[idx], [1, 1],
-                 'ko', label=r'$1\sigma$ interval')
-        plt.plot(mu_axis_values[idx[0]]*np.ones(2), [0, 1], 'k--')
-        plt.plot(mu_axis_values[idx[1]]*np.ones(2), [0, 1], 'k--')
-        plt.xlabel(r'$\mu$')
-        plt.ylabel(r'$-2\log {\cal L}(\mu)/{\cal L}(\hat{\mu})$')
-        plt.title(r'Log-likelihood profile with respect to $\mu$')
+        plt.plot(mu_axis_values[idx], [1, 1], "ko", label=r"$1\sigma$ interval")
+        plt.plot(mu_axis_values[idx[0]] * np.ones(2), [0, 1], "k--")
+        plt.plot(mu_axis_values[idx[1]] * np.ones(2), [0, 1], "k--")
+        plt.xlabel(r"$\mu$")
+        plt.ylabel(r"$-2\log {\cal L}(\mu)/{\cal L}(\hat{\mu})$")
+        plt.title(r"Log-likelihood profile with respect to $\mu$")
 
         # If the log-likelihood ratio is parabolic, sigma_mu is just the half of the
         # difference between the 2 intersection points of the likelihood ratio with 1.
-        sigma_mu = np.diff(mu_axis_values[idx])/2
+        sigma_mu = np.diff(mu_axis_values[idx]) / 2
 
-        plt.plot(mu_axis_values, ((mu_axis_values-1)/sigma_mu)**2, linestyle='-.',
-                 color='tab:gray', label='parabola approximation')
-        plt.legend(facecolor='w')
+        plt.plot(
+            mu_axis_values,
+            ((mu_axis_values - 1) / sigma_mu) ** 2,
+            linestyle="-.",
+            color="tab:gray",
+            label="parabola approximation",
+        )
+        plt.legend(facecolor="w")
         plt.show()
 
         return mini
@@ -118,8 +124,7 @@ def compute_mu(score, weight, saved_info):
 
     # print(nS, nB)
 
-    liste_n = n_bins(score, train_set["weights"],
-                     0.5, (nS, nB, pS, pB, wS, wB))
+    liste_n = n_bins(score, train_set["weights"], 0.5, (nS, nB, pS, pB, wS, wB))
 
     mu_hat = incertitude(liste_n, nS, nB, pS, pB, wS, wB)
 
@@ -143,7 +148,7 @@ def calculate_saved_info(model, train_set):
     def Proba_hist(n, model, train_set):
         """
         Création de deux tableaux : Pb et Ps qui contiendront la probabilité d'avoir du background
-        et du signal respectivement dans chaque Bin 
+        et du signal respectivement dans chaque Bin
         """
         # score = np.array([random() for i in range(len(train_set["data"]))])
 
@@ -156,37 +161,48 @@ def calculate_saved_info(model, train_set):
         for i in range(len(score)):
 
             if train_set["labels"][i] == 1:
-                wS[round((n-1)*score[i])] += train_set['weights'][i]
+                wS[round((n - 1) * score[i])] += train_set["weights"][i]
             else:
-                wB[round((n-1)*score[i])] += train_set['weights'][i]
+                wB[round((n - 1) * score[i])] += train_set["weights"][i]
 
-        S = train_set['weights']*(train_set['labels'])
-        B = train_set['weights']*(1-train_set['labels'])
+        S = train_set["weights"] * (train_set["labels"])
+        B = train_set["weights"] * (1 - train_set["labels"])
 
-        nS, bin = np.histogram(score,
-                               n, weights=S)
-        nB, bin2 = np.histogram(score,
-                                n, weights=B)
+        nS, bin = np.histogram(score, n, weights=S)
+        nB, bin2 = np.histogram(score, n, weights=B)
 
-        pS, bin = np.histogram(score,
-                               n, weights=S, density=True)
-        pS = pS * (bin[1]-bin[0])
-        pB, bin2 = np.histogram(score,
-                                n, weights=B, density=True)
-        pB = pB * (bin[1]-bin[0])
+        pS, bin = np.histogram(score, n, weights=S, density=True)
+        pS = pS * (bin[1] - bin[0])
+        pB, bin2 = np.histogram(score, n, weights=B, density=True)
+        pB = pB * (bin[1] - bin[0])
 
         def histo_plot(score: list, weight: list, label: list, nbins):
             """
             fonction permettant d'afficher les histogrammes
             """
-            dataTot = np.array([[score[i], weight[i], label[i]]
-                                for i in range(len(score))])
+            dataTot = np.array([[score[i], weight[i], label[i]] for i in range(len(score))])
             dataS = dataTot[dataTot[:, 2] == 1]
             dataB = dataTot[dataTot[:, 2] == 0]
-            plt.hist(dataS[:, 0], density=True, weights=dataS[:, 1], bins=nbins, range=(
-                0, 1), color="red", alpha=0.5, label="Signal")
-            plt.hist(dataB[:, 0], density=True, weights=dataB[:, 1], bins=nbins, range=(
-                0, 1), color="blue", alpha=0.5, label="Noise")
+            plt.hist(
+                dataS[:, 0],
+                density=True,
+                weights=dataS[:, 1],
+                bins=nbins,
+                range=(0, 1),
+                color="red",
+                alpha=0.5,
+                label="Signal",
+            )
+            plt.hist(
+                dataB[:, 0],
+                density=True,
+                weights=dataB[:, 1],
+                bins=nbins,
+                range=(0, 1),
+                color="blue",
+                alpha=0.5,
+                label="Noise",
+            )
             plt.xlabel("score")
             plt.ylabel("number of occurence")
             plt.title("Distribution of the score for signal S and noise B")
